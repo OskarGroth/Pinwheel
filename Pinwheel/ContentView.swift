28eafe3b65265af3fdc6a5eb325107581a57b7ca
Oskar Groth <oskar@cindori.org>
2021-04-12 14:38:23 +0200

    Touchups

--- Pinwheel/ContentView.swift
+++ Pinwheel/ContentView.swift
@@ -14,15 +14,14 @@ struct ContentView: View {
     var body: some View {
         WheelView(radius: 50)
             .rotationEffect(animate ? .init(degrees: 360) : .zero)
-            .shadow(color: Color.black.opacity(0.5), radius: 4, y: 2)
+            .shadow(color: Color.black.opacity(0.5), radius: 4, y: 4)
             .onAppear {
-                withAnimation(Animation.linear(duration: 0.75).repeatForever(autoreverses: false)) {
+                withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
                     animate = true
                 }
             }
             .frame(width: 100, height: 100)
-            .padding([.leading, .trailing], 140)
-            .padding([.top, .bottom], 70)
+            .padding(.init(top: 60, leading: 140, bottom: 74, trailing: 140))
     }
 }
 
